5b89f41d8813c52712d93d49a60fa05fbaba2375
jottamar <122304359+jottamar@users.noreply.github.com>
2024-03-21 21:31:17 -0300

    Add files via upload

---
+++ AULA1/js/script.js
@@ -0,0 +1,5 @@
+function teste() {
+    var resultado = 30 + 20;
+    document.getElementById('info').innerHTML = 'O resultado da soma é ' + resultado;
+}
+
---
+++ AULA2/js/script.js
@@ -0,0 +1,4 @@
+function teste() {
+    var resultado = 30 + 20;
+    document.getElementById('info').innerHTML = 'O resultado da soma é ' + resultado;
+}
